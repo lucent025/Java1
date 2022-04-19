79925b6ac7ac2f5ceb9fa7b8544b5d8d748d074b
lucent025 <smile25003@gmail.com>
2022-04-19 23:43:12 +0700

    Update Lab5_2.java

--- Lab5/src/lab5/Lab5_2.java
+++ Lab5/src/lab5/Lab5_2.java
@@ -6,6 +6,7 @@ package lab5;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 public class Lab5_2 {
 
     /**
@@ -13,38 +14,68 @@ public class Lab5_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nhap ten: ");
-        String name = input.nextLine();
-        menu(name);
+        menu();
         
     }
     
-    public static ArrayList<String> menu(ArrayList<String> name) {
-        String subname = "Luc";
-        name.add(subname);
-        return name;
-    }
-    public void nhap() {
+    public static void menu() {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> name = new ArrayList<String>();
-        try {
-            String nhapten = new scanner.nextLine();
-            while(true) {
-                name.add(nhapten);
-            }
+        System.out.print("Nhap ten: ");
+        int luachon = scanner.nextInt();
+        switch(luachon){
+            case 1:
+                nhap();
+                menu();
+                break;
+            case 2:
+                xuat();
+                menu();
+                break;
+            case 3:
+                sapxep();
+                menu();
+                break;
+            case 4:
+                ngaunhien();
+                menu();
+                break;
+            default:
+                menu();
         }
     }
-    public void xuat() {
+    public static ArrayList<String> nhap() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> name = new ArrayList<String>();
+        System.out.print("Nhap ten: ");
+        while(true){
+            String nhapten = scanner.nextLine();
+            name.add(nhapten);
+            
+            System.out.print("Nhap them (N)? ");
+            if(scanner.nextLine().equals("N")) {
+               break; 
+            }
+        }
+        return name;
+    }
+    public static void xuat() {
+     
+        System.out.print(nhap());
         
     }
-    public void sapxep() {
+    public static void sapxep() {
+        
+       Collections.sort(nhap());
+       System.out.print(nhap());
         
     }
-    public void ngaunhien() {
+    public static void ngaunhien() {
+        
+        Collections.shuffle(nhap());
+        System.out.print(nhap());
         
     }
-    public void xoa() {
+    public static void xoa() {
         
     }
 }
