4b5ff07b9054f9dbe0ffdb354c3c03ea68fad03f
lucent025 <smile25003@gmail.com>
2022-04-19 10:02:37 +0700

    Update

---
+++ Lab5/src/lab5/Lab5_2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab5;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Lab5_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    Scanner input = new Scanner(System.in);
+    public static ArrayList<String> menu(ArrayList<String> name) {
+        
+    }
+    public void nhap() {
+        
+    }
+    public void xuat() {
+        
+    }
+    public void sapxep() {
+        
+    }
+    public void ngaunhien() {
+        
+    }
+    public void xoa() {
+        
+    }
+}
