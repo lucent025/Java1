eededd8f04a7659f1cddb119993858d281bf0b74
lucent025 <smile25003@gmail.com>
2022-05-10 15:40:26 +0700

    Update

---
+++ TestWindow/src/testwindow/TestWindow.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package testwindow;
+
+
+
+public class TestWindow {
+
+    
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.initframe();
+    }
+
+}
